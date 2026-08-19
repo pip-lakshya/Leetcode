faf93eb3b0153bdf3be4d1f253c3d147ae7f3f4a
Lakshya Bhandari <bhandarilakshya14@gmail.com>
2026-08-19 09:59:38 +0530

    Time: 70 ms (37.67%), Space: 91.9 MB (13.85%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, int> mp;
 
+        unordered_map<int, unordered_set<int>> mp;
+
+        // Store reserved seats row-wise
         for (auto &v : reservedSeats) {
-            mp[v[0]] |= (1 << v[1]);
+            mp[v[0]].insert(v[1]);
         }
 
         int ans = (n - mp.size()) * 2;
 
-        for (auto &[row, mask] : mp) {
+        for (auto &p : mp) {
 
-            bool left = (mask & ((1 << 2) | (1 << 3) | 
-                                 (1 << 4) | (1 << 5))) == 0;
+            unordered_set<int> &s = p.second;
 
-            bool right = (mask & ((1 << 6) | (1 << 7) | 
-                                  (1 << 8) | (1 << 9))) == 0;
+            // Family in seats 2,3,4,5
+            bool left = !s.count(2) &&
+                        !s.count(3) &&
+                        !s.count(4) &&
+                        !s.count(5);
 
-            bool middle = (mask & ((1 << 4) | (1 << 5) | 
-                                   (1 << 6) | (1 << 7))) == 0;
+            // Family in seats 4,5,6,7
+            bool middle = !s.count(4) &&
+                          !s.count(5) &&
+                          !s.count(6) &&
+                          !s.count(7);
 
-            if (left && right)
+            // Family in seats 6,7,8,9
+            bool right = !s.count(6) &&
+                         !s.count(7) &&
+                         !s.count(8) &&
+                         !s.count(9);
+
+            if (left && right) {
                 ans += 2;
-            else if (left || right || middle)
+            }
+            else if (left || middle || right) {
                 ans += 1;
+            }
         }
 
         return ans;
